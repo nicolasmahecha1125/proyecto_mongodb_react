6545a85b097397b68fa231597d591729189f8302
nicolasmahecha1125 <nicolasmahecha1125@gmail.com>
2026-01-03 22:02:58 -0500

    3 commit 3 de enero del 2026

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header({ design, setDesign, cart }) {
   return (
-    <header className="max-w-6xl mx-auto flex justify-between items-center bg-neutral-800/80 px-6 py-4 rounded-xl mb-8">
+    <header className="w-full mx-auto flex justify-between items-center bg-neutral-800/80 px-6 py-4 rounded-xl mb-8">
       <h1 className="text-2xl font-bold text-amber-500">Bodega Premium</h1>
 
       <nav className="flex gap-2">
